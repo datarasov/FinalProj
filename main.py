cbbd1b6c5a436f11717436f579489b5077f6b524
Dmitry Tarasov <51109854+datarasov@users.noreply.github.com>
2022-11-14 15:18:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+# from fastapi import FastAPI
 # from transformers import pipeline
+# from pydantic import BaseModel
+
+#class Item(BaseModel):
+#    text: str
+
+#app = FastAPI()
+#classifier = pipeline("sentiment-analysis", model="blanchefort/rubert-base-cased-sentiment")
+
+#@app.get("/")
+#def root():
+#    return ("message": "DEMO APPLICATION for AI/DevOps course")
+
+#@app.post("/predict/")
+#def predict(item: Item):
+#    return classifier(item.text)[0]
+
+# ---------- INITIAL CODE ---------------
 # classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
 # classifier("Я обожаю рыбалку!")
 
